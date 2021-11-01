1ccff479b04efcf631ba935b4c791659bfbda7a1
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-11-01 14:56:31 -0400

    refactored arrays.js

--- new_part1/javascript/arrays.js
+++ new_part1/javascript/arrays.js
@@ -1,35 +1,30 @@
 const t = [1, -1, 3];
-
 t.push(5);
 
-console.log(t);
 console.log(t.length);
-console.log(t[0]);
+console.log(t[1]);
 
-t.forEach(value => {
-    console.log(value);
-})
+t.forEach(value => console.log(value));
 
-const t1 = [1, -1, 5];
-const t2 = t1.concat(3);
+// functional programming
 
-console.log("t1: " + t1);
-console.log("t2: " + t2);
+const t2 = [1, -1, 3];
+const t3 = t2.concat(5);
+console.log(t2);
+console.log(t3);
 
-const t3 = [1, 2, 3];
+// map method
 
-const m1 = t3.map(value => value * 2);
-
+const a = [1, 2, 3];
+const m1 = a.map(value => value * 2);
 console.log(m1);
-
-const m2 = t3.map(value => "<li>" + value + "</li>");
-
+const m2 = a.map(value => "<li>" + value + "</li>");
 console.log(m2);
 
-const t4 = [1, 2, 3, 4, 5];
+// destructuring assignment
 
-const [first, second, ...rest] = t4;
-
-console.log("first: " + first);
-console.log("second: " + second);
-console.log("rest: " + rest);
+const b = [1, 2, 3, 4, 5];
+const [first, second, ...rest] = b;
+console.log("first:", first);
+console.log("second:", second);
+console.log("rest:", rest);
