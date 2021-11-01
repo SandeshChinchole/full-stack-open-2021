82363c3b89be23ef35290e50fa58688db9c04d89
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-11-01 15:06:51 -0400

    added objects.js

--- new_part1/javascript/objects.js
+++ new_part1/javascript/objects.js
@@ -1,35 +1,16 @@
-const t = [1, -1, 3];
+const object3 = {
+    name: {
+        first: "Daniel",
+        last: "Radcliffe"
+    },
+    grades: [1, 2, 3, 4],
+    university: "Stanford University"
+};
 
-t.push(5);
+console.log(object3.name);
+const fieldName = "university";
+console.log(object3[fieldName]);
 
-console.log(t);
-console.log(t.length);
-console.log(t[0]);
-
-t.forEach(value => {
-    console.log(value);
-})
-
-const t1 = [1, -1, 5];
-const t2 = t1.concat(3);
-
-console.log("t1: " + t1);
-console.log("t2: " + t2);
-
-const t3 = [1, 2, 3];
-
-const m1 = t3.map(value => value * 2);
-
-console.log(m1);
-
-const m2 = t3.map(value => "<li>" + value + "</li>");
-
-console.log(m2);
-
-const t4 = [1, 2, 3, 4, 5];
-
-const [first, second, ...rest] = t4;
-
-console.log("first: " + first);
-console.log("second: " + second);
-console.log("rest: " + rest);
+object3.location = "NYC";
+object3["email_address"] = "daniel@email.com";
+console.log(object3);
