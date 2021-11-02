f2adefb40fe9615508fa69179a61e15dea783bc5
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-11-02 16:05:29 -0400

    1d conditional rendering

--- new_part1/example/src/App.js
+++ new_part1/example/src/App.js
@@ -1,5 +1,15 @@
 import React, {useState} from "react";
 
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return <div>the app is used by pressing the buttons</div>
+  } else {
+    return <div>button press history: {props.allClicks.join(" ")}</div>
+  }
+};
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
@@ -9,7 +19,7 @@ const App = () => {
     setAllClicks(allClicks.concat("L"));
     setLeft(left + 1);
   };
-
+  
   const handleRightClick = () => {
     setAllClicks(allClicks.concat("R"));
     setRight(right + 1);
@@ -18,10 +28,10 @@ const App = () => {
   return(
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
-      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
