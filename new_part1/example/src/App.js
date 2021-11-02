59098e6d9b31b98311d1a9032af74436e4981dc4
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-11-01 22:53:21 -0400

    completed page re-rendering section

--- new_part1/example/src/App.js
+++ new_part1/example/src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age;
+const App = (props) => {
+  const {counter} = props;
 
   return(
-    <div>
-      <p>hello {name}, you are {age} years old.</p>
-      <p>you were probably born in {bornYear()}.</p>
-    </div>
-  );
-};
-
-const App = () => {
-  const name = "adam";
-  const age = 10;
-
-  return(
-    <div>
-      <Hello name={name} age={age} />
-      <Hello name="dolores" age={10+11} />
-    </div>
+    <div>{counter}</div>
   );
 };
 
