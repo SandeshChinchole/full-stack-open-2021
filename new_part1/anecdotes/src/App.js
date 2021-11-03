11401cb1f26a9be81b102e78dc60504c705b1027
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-11-02 23:24:26 -0400

    completed exercise 1.14

--- new_part1/anecdotes/src/App.js
+++ new_part1/anecdotes/src/App.js
@@ -25,12 +25,30 @@ const App = () => {
     setVotes(vote);
   };
 
+  const maxVotes = () => {
+    const max = Math.max(...votes);
+    return votes.indexOf(max);
+  };
+
   return(
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>Has {votes[selected]} votes</p>
       <button onClick={submitVote}>Vote</button>
       <button onClick={randomAnecdote}>Next Anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      {
+        votes[maxVotes()] === 0 ? 
+        <>
+          <p>No votes yet</p>
+        </>
+        : 
+        <>
+        <p>{anecdotes[maxVotes()]}</p>
+        <p>Has {votes[maxVotes()]} votes</p>
+        </>
+      }
     </div>
   );
 };
